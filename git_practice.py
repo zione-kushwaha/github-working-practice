6558943daa577470b4ebc28405f551305fe13939
Jeevan kumar kushwaha <jeevankushwaha461@gmail.com>
2024-11-26 13:52:51 +0545

    initial commit

---
+++ git_practice.py
@@ -0,0 +1,7 @@
+# program to find sum of all numbers in a list
+def sum_of_list(numbers):
+    return sum(numbers)
+
+# Example usage
+numbers = [1, 2, 3, 4, 5]
+print("The sum is:", sum_of_list(numbers))
